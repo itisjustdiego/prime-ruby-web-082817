d25bc057eb73eccb1ba61dd03d770e37a5f377ca
Diego Dorado <itsjustdiego@gmail.com>
2017-08-17 00:51:27 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
-# Add  code here!
+def prime?(n)
+  if n < 2
+    return false
+  end
+
+#  counter = 2
+#  while counter <= n do
+    for j in (2...n) do
+      if n % j == 0
+        return false
+    end
+  #  if n % counter == 0
+      #counter += 1
+    end
+
+    return true
+  end
